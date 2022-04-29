693ca156bc7c8e47290ba8efc5021dd807f23063
Ryan <jesusc1289@gmail.com>
2022-04-29 16:16:41 -0400

    added one round loop for rps

--- rps.js
+++ rps.js
@@ -11,5 +11,30 @@ function computerPlay() {
         return 'SCISSORS'
     }
   }
-console.log(computerPlay())
+//console.log(computerPlay())
 
+playerChoice = prompt("Rock, Paper, or Scissors")
+playerSelection = playerChoice.toUpperCase();
+computerSelection = computerPlay();
+console.log(playerSelection)
+console.log(computerSelection)
+
+
+if(playerSelection == computerSelection){
+    console.log("tie")
+}
+
+if (
+    (playerSelection == "ROCK" && computerSelection == "SCISSORS") ||
+   (playerSelection == "PAPER" && computerSelection == "ROCK") ||
+   (playerSelection == "SCISSORS" && computerSelection == "PAPER")
+    ){
+   console.log("You win")
+   }
+if (
+        (playerSelection == "ROCK" && computerSelection == "PAPER") ||
+        (playerSelection == "PAPER" && computerSelection == "SCISSORS") ||
+        (playerSelection == "SCISSORS" && computerSelection == "ROCK")
+    ){
+        console.log("You lose")
+        }
