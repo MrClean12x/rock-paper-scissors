cdc423c72e42b2d2f77ea9de3470ebbfee56331b
Ryan <jesusc1289@gmail.com>
2022-04-29 16:14:55 -0400

    added random computer input

---
+++ rps.js
@@ -0,0 +1,15 @@
+
+
+function computerPlay() {
+    let randomNumber = Math.floor(Math.random() * 3)
+    switch (randomNumber) {
+      case 0:
+        return 'ROCK'
+      case 1:
+        return 'PAPER'
+      case 2:
+        return 'SCISSORS'
+    }
+  }
+console.log(computerPlay())
+
