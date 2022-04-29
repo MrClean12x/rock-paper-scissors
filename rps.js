2b8b76f68707bab2ddd2995f9e54e1d120b6adb1
Ryan <jesusc1289@gmail.com>
2022-04-29 18:13:29 -0400

    added loop to play multiple rounds, until 5 wins

--- rps.js
+++ rps.js
@@ -1,4 +1,7 @@
 
+let playerScore = 0
+let computerScore = 0
+let roundWinner = ""
 
 function computerPlay() {
     let randomNumber = Math.floor(Math.random() * 3)
@@ -13,31 +16,51 @@ function computerPlay() {
   }
 //console.log(computerPlay())
 
-playerChoice = prompt("Rock, Paper, or Scissors")
-playerSelection = playerChoice.toUpperCase();
-computerSelection = computerPlay();
-console.log(playerSelection)
-console.log(computerSelection)
+
 
 function playRound(playerSelection, computerSelection){
+
+    
     if(playerSelection == computerSelection){
-        return "tie"
+        roundWinner =  "tie"
     }
 
     if (
         (playerSelection == "ROCK" && computerSelection == "SCISSORS") ||
         (playerSelection == "PAPER" && computerSelection == "ROCK") ||
         (playerSelection == "SCISSORS" && computerSelection == "PAPER")
-    ){
-        return "you win"
+    ){  playerScore++
+        roundWinner =  "player wins"
     }
     if (
         (playerSelection == "ROCK" && computerSelection == "PAPER") ||
         (playerSelection == "PAPER" && computerSelection == "SCISSORS") ||
         (playerSelection == "SCISSORS" && computerSelection == "ROCK")
-    ){
-        return "you lose"
+    ){  computerScore++
+        roundWinner =  "computer wins"
     }
 }
 
-console.log(playRound(playerSelection,computerSelection));
+
+
+
+
+
+
+while (true){
+    let playerChoice = prompt("Rock, Paper, or Scissors");
+    let playerSelection = playerChoice.toUpperCase();
+    let computerSelection = computerPlay();
+    playRound(playerSelection,computerSelection)
+    console.log(playerScore)
+    console.log(computerScore)
+    console.log(roundWinner)
+    if (playerScore == 5)
+        //console.log("You win!");
+        break;
+    if (computerScore == 5)
+        //console.log("You lose.")
+        break;
+}
+
+
